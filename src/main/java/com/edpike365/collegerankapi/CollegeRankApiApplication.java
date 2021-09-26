58029a3232d6c04e658a0d285850dd53e1a2b8d4
Ed Pike <edpike365@gmail.com>
2021-09-26 22:38:19 +0000

    first commit, added .devcontainer,  spring boot

---
+++ src/main/java/com/edpike365/collegerankapi/CollegeRankApiApplication.java
@@ -0,0 +1,13 @@
+package com.edpike365.collegerankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeRankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeRankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edpike365/collegerankapi/CollegeRankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edpike365.collegerankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeRankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
